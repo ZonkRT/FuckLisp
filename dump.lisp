53dd6133494bf1412242c655fe8be13ce335e69a
Ryan <rscoopo@gmail.com>
2017-12-05 18:40:26 -0500

    Update dump.lisp

--- dump.lisp
+++ dump.lisp
@@ -59,3 +59,23 @@
       (ancestors(second(person-parents p))))))
      
 ;End of Nick
+
+;Ryan
+
+(defun cousin(p1 p2)
+	(setq l1 '(ancestor(p1)))
+	(setq l2 '(ancestor(p2)))
+	(setq bool1 nil) ;;The three boolean values: common, direct, result
+	(setq bool2 nil) 
+	(setq bool3 nil)
+	(dolist (name1 'l1)
+		(dolist (name2 'l2)
+			(when (= name1 name2) (setq bool1 t))))
+	(dolist (n 'l1)
+		(when (= n p2-name) (setq bool2 t)))
+	(dolist (n2 'l2)
+		(when (= n2 p1-name) (setq bool2 t)))
+	(when (and (not (not bool)) (not (not bool2)) (setq bool3 t)))
+	(return bool3))
+
+;End of Ryan
